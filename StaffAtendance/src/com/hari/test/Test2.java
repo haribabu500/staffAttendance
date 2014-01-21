18aba96f4d95c85e0c2802480f1d59206110aaac
hari <vijay.stha@hotmail.co.uk>
2014-01-21 11:21:56 -0800

    test commit

--- StaffAtendance/src/com/hari/test/Test2.java
+++ StaffAtendance/src/com/hari/test/Test2.java
@@ -7,6 +7,7 @@ import javax.persistence.Persistence;
 import com.hari.model.Test;
 
 public class Test2 {
+	//hari krishna shrestha
 	public static void main(String[] args) {
 		EntityManager em=Persistence.createEntityManagerFactory("STAFF_ATTENDANCE").createEntityManager();
 		Test test=new Test();
