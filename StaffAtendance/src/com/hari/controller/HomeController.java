6916bf9474863437fa37d8458e4a57479b4171dd
hari <vijay.stha@hotmail.co.uk>
2014-01-21 00:11:13 -0800

    home commit

---
+++ StaffAtendance/src/com/hari/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.hari.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/home")
+	public ModelAndView home(){
+		ModelAndView mav=new ModelAndView("/home");
+		return mav;
+	}
+}
