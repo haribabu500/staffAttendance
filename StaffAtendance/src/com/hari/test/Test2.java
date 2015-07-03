32a92cf65a58b36c835a4bad07f9c1bae466ebb6
hari <vijay.stha@hotmail.co.uk>
2015-07-03 12:16:32 +0545

    hhh

--- StaffAtendance/src/com/hari/test/Test2.java
+++ StaffAtendance/src/com/hari/test/Test2.java
@@ -12,18 +12,22 @@ import com.hari.service.EasServiceManager;
 
 public class Test2 {
 	//hari krishna shrestha
-	public static void main2(String[] args) {
+	public static void main(String[] args) {
 		EntityManager entityManager=Persistence.createEntityManagerFactory("STAFF_ATTENDANCE").createEntityManager();
 		entityManager.getTransaction().begin();
 		List<Staff> list=entityManager.createQuery("select s from Staff s").getResultList();
-		System.out.println(EasServiceManager.getStaffTable(list));
+//		List<Staff> list=entityManager.createQuery("select s from Staff s where s.firstName like '%h%' or s.lastName like '%h%'").getResultList();
+//		System.out.println(EasServiceManager.getStaffTable(list));
 		for (Staff object : list) {
-			System.out.println((object.getStartDate()));
+			System.out.println((">>>>>"+object.getUsernam()+">>>"+object.getPassword()+">>---"+object.getStartDate()));
 		}
-		Staff staff=entityManager.find(Staff.class, 255);
-		System.out.println(staff.getFirstName());
+//		Staff staff=entityManager.find(Staff.class, 255);
+//		System.out.println(staff.getFirstName());
+		
+//		EasServiceManager.remove("7402");
+		System.out.println("done");
 	}
-	public static void main(String[] args) {
+	public static void main1(String[] args) {
 		char arr[]={'a','b','c','d','e','f','g','h','i','j'};
 		/*for(int i=0;i<arr.length;i=i+3){
 			for(int j=i;j<i+3;j++){
@@ -32,5 +36,6 @@ public class Test2 {
 			System.out.println();
 		}*/
 		System.out.println(arr[9]);
+		
 	}
 }
